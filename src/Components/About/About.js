0e29ab646beed6ac56485355b6c5caa6ea000e16
Ghoudi Zakaria <zakariaghoudi1@gmail.com>
2026-07-13 18:23:04 +0100

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -42,11 +42,11 @@ const About = () => {
 
         <div className="skills-grid"> {/* array of icons of skills */}
           {[
-            {icon: <DiHtml5 color="#e34c26" size="60px" /> },
-            {icon: <DiCss3 color="#2965f1" size="60px" /> },
-            {icon: <DiJavascript1 color="#f7df1e" size="60px" /> },
-            {icon: <DiReact color="#61dafb" size="60px" /> },
-            {icon: <DiNodejs color="#68a063" size="60px" /> },
+            {icon: <DiHtml5 color="#e34c26" size="55px" /> },
+            {icon: <DiCss3 color="#2965f1" size="55px" /> },
+            {icon: <DiJavascript1 color="#f7df1e" size="55px" /> },
+            {icon: <DiReact color="#61dafb" size="55px" /> },
+            {icon: <DiNodejs color="#68a063" size="55px" /> },
             {icon: <DiGithub color="#333" size="60px" /> },
             {icon: <DiResponsive color="#ff9800" size="90px" /> },
           ].map((skill, index) => (
