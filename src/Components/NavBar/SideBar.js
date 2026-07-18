08828b72149b378229b45a13da46a111db192059
Ghoudi Zakaria <zakariaghoudi1@gmail.com>
2026-07-18 18:22:46 +0100

    Update SideBar.js

--- src/Components/NavBar/SideBar.js
+++ src/Components/NavBar/SideBar.js
@@ -59,6 +59,24 @@ const FolderOpenIcon = (props) => (
   </svg>
 )
 
+const BlogIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+    <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+  </svg>
+)
+
 const MailIcon = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -132,6 +150,7 @@ const SideBar = ({ isDarkMode, toggleTheme }) => {
     { path: "/", label: "Home", icon: HomeIcon },
     { path: "/about", label: "About", icon: UserIcon },
     { path: "/projects", label: "Projects", icon: FolderOpenIcon },
+    { path: "/blog", label: "Blog", icon: BlogIcon },
     { path: "/contact", label: "Contact", icon: MailIcon },
   ]
 
